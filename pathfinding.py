ada827e9db990e5a13efa882ca8183753179e9cd
Neuroszima <mat.anuszewski1@gmail.com>
2021-06-22 08:56:32 +0200

    added 3d pathfinding and refactored code into percolation classes

--- pathfinding.py
+++ pathfinding.py
@@ -2,20 +2,20 @@ from math import sqrt
 from copy import deepcopy
 
 
-def find_path_2D(surface, start=None, end=None):
+def find_path_2D(lattice, start=None, end=None):
     """
     always tries to find path from top left to bottom right [0,0] > [max_x,max_y]
     Tries to imitate A* pathfinding,
 
-    :param surface: a lattice to find path through
+    :param lattice: a lattice to find path through
     :param start: point at algorithm starts from, defaults at (0, 0)
     :param end: point to reach, default is (len(lattice), len(lattice_row))
-    :return:
+    :return: True/False value, whether a path between points exist
     """
     if start is None:
         start = [0, 0]
     if end is None:
-        end = [len(surface[0]), len(surface)]
+        end = [len(lattice[0]), len(lattice)]
 
     max_x: int = end[0]
     max_y: int = end[1]
@@ -24,20 +24,23 @@ def find_path_2D(surface, start=None, end=None):
     route = [start]
     possible = []
 
-    step = 0
     while pos != [max_y-1, max_x-1]:
-        step += 1
+        # north/south (up/down), west/east (left/right) order
         dist_n, dist_s, dist_w, dist_e = -1, -1, -1, -1
-        # test surroundings
-        if pos[0] > 0:
-            dist_n = sqrt((max_x-(pos[0]-1))**2 + (max_y-pos[1])**2) * int(surface[pos[0]-1][pos[1]]) - 0.001
-        if pos[0] < max_y-1:
-            dist_s = sqrt((max_x-(pos[0]+1))**2 + (max_y-pos[1])**2) * int(surface[pos[0]+1][pos[1]]) - 0.001
-        if pos[1] > 0:
-            dist_w = sqrt((max_x-pos[0])**2 + (max_y-(pos[1]-1))**2) * int(surface[pos[0]][pos[1]-1]) - 0.001
-        if pos[1] < max_x-1:
-            dist_e = sqrt((max_x-pos[0])**2 + (max_y-(pos[1]+1))**2) * int(surface[pos[0]][pos[1]+1]) - 0.001
 
+        # test surroundings
+        # False value immidietaly causes expression to equal 0, substracting -0.001 causes it to be viable
+        # for < 0 checks
+        if pos[0] > 0:
+            dist_n = sqrt((max_x-(pos[0]-1))**2 + (max_y-pos[1])**2) * int(lattice[pos[0] - 1][pos[1]]) - 0.001
+        if pos[0] < max_y-1:
+            dist_s = sqrt((max_x-(pos[0]+1))**2 + (max_y-pos[1])**2) * int(lattice[pos[0] + 1][pos[1]]) - 0.001
+        if pos[1] > 0:
+            dist_w = sqrt((max_x-pos[0])**2 + (max_y-(pos[1]-1))**2) * int(lattice[pos[0]][pos[1] - 1]) - 0.001
+        if pos[1] < max_x-1:
+            dist_e = sqrt((max_x-pos[0])**2 + (max_y-(pos[1]+1))**2) * int(lattice[pos[0]][pos[1] + 1]) - 0.001
+
+        # check adjacent (later - a) cells for illogical distances and sort them in order of distance from end
         d = [_ for _ in [
             [[pos[0]-1, pos[1]], dist_n],
             [[pos[0]+1, pos[1]], dist_s],
@@ -45,7 +48,7 @@ def find_path_2D(surface, start=None, end=None):
             [[pos[0], pos[1]+1], dist_e]
         ] if _[1] > 0]
 
-        d = sorted(d, key=lambda s: s[1])
+        d = sorted(d, key=lambda a: a[1])
         x, selected, changed = 0, 0, False
         while x < len(d):
             if d[x][0] not in visited:
@@ -69,3 +72,138 @@ def find_path_2D(surface, start=None, end=None):
                 return False
 
     return True
+
+
+def find_path_3D(lattice, start=None, end=None, verbose=False):
+    """
+    exact same algorithm as above, however one that is designed for 3 dimensional space
+
+    :param lattice: 3D array to look through
+    :param start: a lattice starting point for algorithm
+    :param end: point we try to get to
+    :return: True/False value, whether a path between points exist
+    """
+    if start is None:
+        start = [0, 0, 0]
+    if end is None:
+        end = [len(lattice[0][0]), len(lattice[0]), len(lattice)]
+
+    max_x: int = end[0]
+    max_y: int = end[1]
+    max_z: int = end[2]
+
+    pos = start
+    visited = [start]
+    route = [start]
+    possible = []
+
+    steps = 0
+    while pos != [max_z-1, max_y-1, max_x-1] and steps < 300:
+        steps += 1
+        # north/south, west/east, back/front order
+        dist_n, dist_s, dist_w, dist_e, dist_b, dist_f = -1, -1, -1, -1, -1, -1
+
+        if pos[0] > 0:
+            dist_n = sqrt(
+                (max_x - (pos[0]-1))**2 +
+                (max_y - pos[1])**2 +
+                (max_z - pos[2])**2) * int(lattice[pos[0] - 1][pos[1]][pos[2]]) - 0.001
+        if pos[0] < max_z-1:
+            dist_s = sqrt(
+                (max_x - (pos[0] + 1))**2 +
+                (max_y - pos[1])**2 +
+                (max_z - pos[2])**2) * int(lattice[pos[0] + 1][pos[1]][pos[2]]) - 0.001
+        if pos[1] > 0:
+            dist_w = sqrt(
+                (max_x - pos[0])**2 +
+                (max_y - (pos[1]-1))**2 +
+                (max_z - pos[2])**2) * int(lattice[pos[0]][pos[1] - 1][pos[2]]) - 0.001
+        if pos[1] < max_y - 1:
+            dist_e = sqrt(
+                (max_x - pos[0])**2 +
+                (max_y - (pos[1]+1))**2 +
+                (max_z - pos[2])**2) * int(lattice[pos[0]][pos[1] + 1][pos[2]]) - 0.001
+        if pos[2] > 0:
+            dist_b = sqrt(
+                (max_x - pos[0])**2 +
+                (max_y - pos[1])**2 +
+                (max_z - (pos[2]-1))**2) * int(lattice[pos[0]][pos[1]][pos[2] - 1]) - 0.001
+        if pos[2] < max_x - 1:
+            dist_f = sqrt(
+                (max_x - pos[0])**2 +
+                (max_y - pos[1])**2 +
+                (max_z - (pos[2]+1))**2) * int(lattice[pos[0]][pos[1]][pos[2] + 1]) - 0.001
+
+        d = [_ for _ in [
+            [[pos[0] - 1, pos[1], pos[2]], dist_n],
+            [[pos[0] + 1, pos[1], pos[2]], dist_s],
+            [[pos[0], pos[1] - 1, pos[2]], dist_w],
+            [[pos[0], pos[1] + 1, pos[2]], dist_e],
+            [[pos[0], pos[1], pos[2] - 1], dist_b],
+            [[pos[0], pos[1], pos[2] + 1], dist_f]
+        ] if _[1] > 0]
+
+        d = sorted(d, key=lambda a: a[1])
+        x, selected, changed = 0, 0, False
+        while x < len(d):
+            if d[x][0] not in visited:
+                if not changed:
+                    selected, changed = x, True
+                elif changed:
+                    r = deepcopy(route)
+                    r.append(d[x][0])
+                    possible.append(r)
+            x += 1
+
+        if changed:
+            route.append(d[selected][0])
+            visited.append(pos)
+            pos = d[selected][0]
+            # print(f"step {steps}, route: {route}, \nvisited: {len(visited)}\n" +
+            #       f"other routes: {len(possible)}")
+        else:
+            try:
+                route = possible.pop()
+                pos = route[-1]
+            except IndexError:
+                return False
+
+    return True
+
+
+def print_lat_3d(lat):
+    print("3d lattice")
+    for surface in lat:
+        for line in surface:
+            print(line)
+        print('\n')
+
+
+if __name__ == '__main__':
+    from random import seed
+    from percolation import (
+        lattice_gen_2D as lat_2d,
+        lattice_gen_3D as lat_3d
+    )
+
+    # test against known case
+    seed(200)
+    # no possible path
+    l_2d = [[True] + line + [True] for line in lat_2d()]
+    # guaranteed path
+    l_2d_2 = [[True] + line + [True] for line in lat_2d(p = 0.7)]
+
+    assert (find_path_2D(l_2d)) == False
+    assert (find_path_2D(l_2d_2)) == True
+
+    # no possible path
+    l_3d = [[[True] + line + [True] for line in surface]
+              for surface in lat_3d()]
+    # guaranteed path with use of 3 dimensions
+    l_3d_2 = [[[True] + line + [True] for line in surface]
+              for surface in lat_3d(p = 0.4)]
+
+    assert find_path_3D(l_3d) == False
+    assert find_path_3D(l_3d_2) == True
+
+    print_lat_3d(l_3d_2)
--- percolation.py
+++ percolation.py
@@ -1,5 +1,7 @@
 from random import random
-from pathfinding import find_path_2D
+from copy import deepcopy
+
+from pathfinding import find_path_2D, find_path_3D
 
 
 def lattice_gen_2D(x=10, y=10, p=0.1):
@@ -7,33 +9,104 @@ def lattice_gen_2D(x=10, y=10, p=0.1):
             for _ in range(y)]
 
 
-def lattice_gen_3D(x=10, y=10, z=10, p=0.1):
+def lattice_gen_3D(x=6, y=6, z=6, p=0.1):
     return [[[True if random() < p else False for _ in range(x)]
              for _ in range(y)]
-             for _ in range(z)]
+            for _ in range(z)]
 
 
-if __name__ == "__main__":
-    #warning, program runs for a lot of tmie!
+class Percolation:
+    def __init__(self, starting_size=4, end_size=10,
+                 start_propablity=0.2, end_propability=0.6, step=0.05,
+                 samples=70):
+        self.s_s = starting_size
+        self.e_s = end_size
+        self.s_p = start_propablity
+        self.e_p = end_propability
+        self._step = step
+        self._samples = samples
+        self.__data = []
 
-    data = []
-    propabilities = [0.1 + 0.02 * _ for _ in range(5, 25)]
-    sizes = list(range(4, 14))
-    samples_per_size = 100
+    def get_propablity_set(self):
+        power = 0
+        st = deepcopy(self._step)
+        for _ in range(5):
+            if st < 1:
+                st *= 10
+                power += 1
+            else:
+                break
 
-    #2 dimensional lattice
-    for size in sizes:
-        size_set = []
-        for pr in propabilities:
-            prop_data = 0
-            for i in range(samples_per_size):
-                lat = lattice_gen_2D(size, size, pr)
-                #horizontal test
-                if find_path_2D([[True] + line + [True] for line in lat]): prop_data+=1
-            size_set.append(prop_data)
-        data.append(size_set)
+        p_count = int(int(self.e_p * (10 ** power) - self.s_p * (10 ** power)) / int(st))
+        return [self.s_p + self._step * p for p in range(p_count)]
 
-    #printing results
-    for size, data_row in zip(sizes, data):
-        for pr, counts in zip(propabilities, data_row):
-            print(f"size: {size}, prop: {pr}, counts:{counts}")
+    def get_results(self):
+        return self.__data
+
+    def start_sampling(self):
+        pass
+
+    def plot(self):
+        pass
+
+
+class Percolation_2D(Percolation):
+    def __init__(self, starting_size=4, end_size=13,
+                 start_propablity=0.2, end_propability=0.65, step=0.03,
+                 samples=100):
+        super(Percolation_2D, self).__init__(starting_size=starting_size,
+                                             end_size=end_size,
+                                             start_propablity=start_propablity,
+                                             end_propability=end_propability,
+                                             step=step, samples=samples)
+        self.__data = []
+
+    def start_sampling(self):
+        sizes = list(range(self.s_s, self.e_s))
+        for size in sizes:
+            size_set = []
+            print("size " + str(size))
+            for pr in self.get_propablity_set():
+                success_rate = 0
+                for i in range(self._samples):
+                    lat = lattice_gen_2D(size, size, pr)
+                    # horizontal test
+                    if find_path_2D([[True] + line + [True] for line in lat]): success_rate += 1
+                size_set.append([size, pr, success_rate])
+            self.__data.append(size_set)
+
+
+class Percolation_3D(Percolation):
+    def __init__(self, starting_size=4, end_size=10,
+                 start_propablity=0.2, end_propability=0.6, step=0.05,
+                 samples=100):
+        super(Percolation_3D, self).__init__(starting_size=starting_size,
+                                             end_size=end_size,
+                                             start_propablity=start_propablity,
+                                             end_propability=end_propability,
+                                             step=step, samples=samples)
+        self.__data = []
+
+    def start_sampling(self):
+        sizes = list(range(self.s_s, self.e_s))
+        for size in sizes:
+            print("size" + str(size))
+            size_set = []
+            for pr in self.get_propablity_set():
+                success_rate = 0
+                for i in range(self._samples):
+                    lat = lattice_gen_3D(size, size, size, pr)
+                    # horizontal test
+                    if find_path_3D([[[True] + line + [True]
+                                      for line in surface]
+                                     for surface in lat]):
+                        success_rate += 1
+                size_set.append([size, pr, success_rate])
+            self.__data.append(size_set)
+
+
+if __name__ == '__main__':
+    perc_2d = Percolation_2D()
+    perc_2d.start_sampling()
+    perc_3d = Percolation_3D()
+    perc_3d.start_sampling()
